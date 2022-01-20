8ae31e0cd13ccacad47dfc1fd68b15270203caf4
CanonsLab <97261493+CanonsLab@users.noreply.github.com>
2022-01-20 15:38:39 -0700

    contains director class

---
+++ hilo_game/director.py
@@ -0,0 +1,14 @@
+from hilo_game import card
+
+class director:
+    def __init__(self):
+        pass
+    def start_game(self):
+        pass
+    def get_inputs(self):
+        pass
+    def do_updates(self):
+        pass
+    def do_outputs(self):
+        pass
+    
