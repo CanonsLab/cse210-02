8313079c059e48d5fc57fc52bd1298151c2e7bf8
BeatrizPaola <tiz.sandoval@gmail.com>
2022-01-21 22:44:10 -0300

    Added code for the game´s restrictions on the points based on the next random card

--- hilo_game/card.py
+++ hilo_game/card.py
@@ -4,9 +4,27 @@ import random
 class Card:
 
     def __init__(self):
-        self.value = 0
-        self.points = 0
+        """Constructs a new instance of card.
+
+        Args:
+            self (card): An instance of card.
+        """
+        self.value = random(1,13)
+        self.points = 300
 
     def pick(self):
-        self.value = random.randint(1, 6)
-        self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
+        next_card = random.randint(1, 13)
+        guess_card = input("Higher or lower? [h/l] ")
+        if guess_card=="l" and self.value > next_card:
+            self.points+=100
+        else:
+            self.points-=75
+
+        if guess_card=="h" and self.value<next_card:
+            self.points+=100
+        else: 
+            self.points-=75
+
+
+
+        
--- hilo_game/director.py
+++ hilo_game/director.py
@@ -1,14 +1,66 @@
 from hilo_game import card
+import random
 
 class director:
     def __init__(self):
-        pass
+        """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        card (int): A random number between 1 and 13.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
+        self.next_card = random.radint(1,13)
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 0
+
     def start_game(self):
-        pass
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
     def get_inputs(self):
-        pass
+        """Ask the user if they want to play again
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+        play_again=input("Play again? [y/n] ")
+        self.is_playing = (play_again =="y") and self.points> 0
+       
     def do_updates(self):
-        pass
+        if not self.is_playing:
+            return 
+        next_card=self.next_card.pick()
+        self.score += card.points 
+        self.total_score += self.score
+
     def do_outputs(self):
-        pass
+        """Displays the next card value. Also display the score. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+        next_card = 0
+        
+        next_card=self.next_card.pick()
+        next_card= f"{next_card} "
+
+        print(f"Next card was: {next_card}")
+        print(f"Your score is: {self.total_score}\n")
+        self.is_playing == (self.score > 0)
     
