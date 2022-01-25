4c863225d9650e37506b00741b9831ac9d5dbcf8
BeatrizPaola <tiz.sandoval@gmail.com>
2022-01-25 13:19:47 -0300

    Code added according the rules 9,8,10

--- director.py
+++ director.py
@@ -7,19 +7,27 @@ class Director:
         Attributes
             self (Card) = call the Class Card
             self (win) = Earns points if the player guessed correctly
+            self (is playing) = Whether or not the game is being played.
             self (lose) = Loses points if the player guessed incorrectly 
         """
 
         self.card = Card()
+        self.is_playing = True
         self.win = 100
         self.lose = -75
         
     def start_game(self):
         """Execute the functions to run the game"""
 
-        self.get_inputs()
-        self.do_updates()
-        self.do_outputs()
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+            if self.card.points<=0:
+                print("Game is over")
+            play_again=input("Play again? [y/n]: ")
+            self.is_playing = (play_again =="y") and self.card.points> 0
+            
 
     def get_inputs(self):
         """Display the current card.
@@ -28,14 +36,14 @@ class Director:
             self (guess_card) = Get the input of the user.
         """
 
-        print(f"The card is: {self.card.current}")
+        print(f"\nThe card is: {self.card.current}")
         self.guess_card = input("Higher or lower? (h/l): ")
        
     
         
     def do_updates(self):
         """Add or subtract points to the user."""
-
+            
         if self.card.current > self.card.next and self.guess_card == "l" \
         or self.card.current < self.card.next and self.guess_card == "h":
             self.card.points += self.win 
@@ -49,3 +57,4 @@ class Director:
         """Display to the user the results."""
         print(f"Next card was: {self.card.next}")
         print(f"Your score is: {self.card.points}")
+        
